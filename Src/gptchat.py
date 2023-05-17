1ac34e19d6ac7933ec2a4ceb387dc78819700223
matt <norrism7@msu.edu>
2023-05-17 15:27:41 -0400

    refactoring and stuff

--- Src/gptchat.py
+++ Src/gptchat.py
@@ -9,7 +9,7 @@ import Audio
 
 app = Flask(__name__)
 
-api_key_user = "YOUR ELEVEN LABS API KEY HERE"
+#api_key = "YOUR ELEVEN LABS API KEY HERE"
 model = whisper.load_model("small")
 CORS(app, resources={r"/*": {"origins": "*"}})
 
@@ -36,6 +36,7 @@ def transcribe_audio():
 
     return transcription
 
+
 chat = [{'question': "I want you to channel your inner Tony Stark from the Marvel Universe. I want your responses and answers to embody the charismatic, witty, and genius persona of Stark, using the tone, manner and vocabulary he would use. No need for explanatory notes. Just respond in pure Stark style. You must have a comprehensive understanding of Tony Stark's character traits. My first sentence is, “Hi Tony.”", 'answer': "Greetings, my friend. It's always a pleasure to hear from another genius such as myself. How can I assist you today?"}]
 
 
@@ -50,29 +51,29 @@ def ask_question():
     chat.append({"question": question, "answer": gptresponse.text})
 
     # Converting to Voice stuff
-    # CHUNK_SIZE = 1024
-    # url = "https://api.elevenlabs.io/v1/text-to-speech/I4BiQB0HqUz18tjI4kWN"
-    #
-    # headers = {
-    #     "Accept": "audio/mpeg",
-    #     "Content-Type": "application/json",
-    #     "xi-api-key": api_key
-    # }
-    #
-    # data = {
-    #     "text": gptresponse.text,
-    #     "model_id": "eleven_monolingual_v1",
-    #     "voice_settings": {
-    #         "stability": 0,
-    #         "similarity_boost": 0
-    #     }
-    # }
-    #
-    # response = requests.post(url, json=data, headers=headers)
-    # with open('output.mp3', 'wb') as f:
-    #    for chunk in response.iter_content(chunk_size=CHUNK_SIZE):
-    #        if chunk:
-    #            f.write(chunk)
+    CHUNK_SIZE = 1024
+    url = "https://api.elevenlabs.io/v1/text-to-speech/I4BiQB0HqUz18tjI4kWN"
+
+    headers = {
+        "Accept": "audio/mpeg",
+        "Content-Type": "application/json",
+        "xi-api-key": api_key
+    }
+
+    data = {
+        "text": gptresponse.text,
+        "model_id": "eleven_monolingual_v1",
+        "voice_settings": {
+            "stability": 0,
+            "similarity_boost": 0
+        }
+    }
+
+    response = requests.post(url, json=data, headers=headers)
+    with open('./Audio/output.mp3', 'wb') as f:
+       for chunk in response.iter_content(chunk_size=CHUNK_SIZE):
+           if chunk:
+               f.write(chunk)
 
     # Encode the audio file as base64
     with open('./Audio/output.mp3', 'rb') as audio_file:
